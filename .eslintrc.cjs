e538a2ccdaa2df85e69d70aa5e499236ca63279d
jcalhinho <jc.alhinho@nanocorp.fr>
2024-05-04 21:15:15 +0200

    change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs',"node_modules/*",],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  plugins: ['react-refresh','@typescript-eslint'],
+  // rules: {
+  //   'react-refresh/only-export-components': [
+  //     'warn',
+  //     { allowConstantExport: true },
+  //   ],
+  // },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-
-
-import './index.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './index.css'; // Assurez-vous que votre fichier CSS est bien importé
 
 function App() {
- 
-
   return (
-    <div className="body">
-      
-      <div className="flex justify-center">
-        hehe soon!!
-       </div>
-    </div>
-  )
+   
+      <Router>
+        <div className="body">
+          <nav className="p-4">
+            <div className="flex justify-center space-x-4">
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/parkings-velo">Parkings Vélo</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/evenements-culturels">Événements Culturels</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/centres-preparation">Centres de Préparation</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/aide-volontaires">Aide aux Volontaires</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/sites-competition">Sites de Compétition</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/psa-snm">PSA SNM</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/boutiques-officielles">Boutiques Officielles</Link></button>
+              <button className="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-lg cursor-pointer shadow-neumorphism transition duration-200  active:shadow-neumorphism-active"><Link to="/poi-sites">POI Sites</Link></button>
+            </div>
+          </nav>
+          <Routes>
+            <Route path="/parkings-velo" element={<>parking</>} />
+            <Route path="/evenements-culturels" element={<>evenements-culturels</>} />
+            <Route path="/centres-preparation" element={<>centres-preparation</>} />
+            <Route path="/aide-volontaires" element={<>aide-volontaires</>} />
+            <Route path="/sites-competition" element={<>sites-competition</>} />
+            <Route path="/psa-snm" element={<>psa-snm</>} />
+            <Route path="/boutiques-officielles" element={<>boutiques-officielles</>} />
+            <Route path="/poi-sites" element={<>poi-sites</>} />
+          </Routes>
+        </div>
+      </Router>
+  );
 }
 
-export default App
+export default App;
